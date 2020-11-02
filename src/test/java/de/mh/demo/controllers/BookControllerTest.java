4bd405d7cd669edd517edfc1dac2c54bc8e9390e
uid09552 <max.rgbg@gmail.com>
2020-11-02 17:05:23 +0100

    some tests

--- src/test/java/de/mh/demo/controllers/BookControllerTest.java
+++ src/test/java/de/mh/demo/controllers/BookControllerTest.java
@@ -1,5 +1,6 @@
 package de.mh.demo.controllers;
 
+import de.mh.demo.domain.Book;
 import de.mh.demo.repositories.BookRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,10 +9,15 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class BookControllerTest {
     BookController bookController;
@@ -26,10 +32,22 @@ class BookControllerTest {
 
     @Test
     void getBooks() throws Exception {
+        HashSet<Book> books = new HashSet<>();
+        var book = new Book();
+        book.setTitle("mybook");
+        book.setId(1L);
+        book.setIsbn("myisbm");
+        books.add(book);
+        when(bookRepository.findAll()).thenReturn(books);
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
-        mockMvc.perform(get("/books"))
-                .andExpect(status().isOk())
-        .andExpect(view().name("books/list"));
 
+        var result = mockMvc.perform(get("/books"))
+                .andExpect(status().isOk())
+        .andExpect(view().name("books/list"))
+        .andDo(print()).andReturn();
+
+        HashSet<Book> resultBookModel = (HashSet<Book>)result.getModelAndView().getModel().get("books");
+        assertEquals(1,resultBookModel.size());
+       System.out.println(result);
     }
 }
