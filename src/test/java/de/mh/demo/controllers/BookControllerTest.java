be01fd311672d88bd3e39d5c31cebd06c1aadb29
uid09552 <max.rgbg@gmail.com>
2020-11-02 21:10:05 +0100

    some tests

--- src/test/java/de/mh/demo/controllers/BookControllerTest.java
+++ src/test/java/de/mh/demo/controllers/BookControllerTest.java
@@ -6,24 +6,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import javax.annotation.Resource;
 import java.util.HashSet;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+//@SpringBootTest
 class BookControllerTest {
     BookController bookController;
     @Mock
     BookRepository bookRepository;
-
+    @Resource
+    private WebApplicationContext webApplicationContext;
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -39,11 +46,13 @@ class BookControllerTest {
         book.setIsbn("myisbm");
         books.add(book);
         when(bookRepository.findAll()).thenReturn(books);
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
-
+        //MockMvc mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         var result = mockMvc.perform(get("/books"))
                 .andExpect(status().isOk())
         .andExpect(view().name("books/list"))
+                .andExpect(model().attribute("books", hasSize(1)))
+                .andExpect(content().string(containsString("mybook")))
         .andDo(print()).andReturn();
 
         HashSet<Book> resultBookModel = (HashSet<Book>)result.getModelAndView().getModel().get("books");
